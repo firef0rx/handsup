b92a27be7640c9cf44b41d9f4fd8c31ee522aa7b
FireF0RX <96952541+firef0rx@users.noreply.github.com>
2023-02-02 12:26:27 +0200

    v3
    New functions:
    Compatibility with mythic_notify and okokNotify
    Optimization
    Custom language

---
+++ gls-handssur/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'FireF0RX#6598'
+name 'Gliese HandsUp & Surrender'
+description 'HandsUp & Surrender Script, Discord: https://dsc.gg/fvmarket'
+
+client_scripts {
+   'client/client.lua'
+}
+
+exports {
+   'hands',
+   'surrender'
+}
